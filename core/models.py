a360121ede89b8d3224e53d62632bbbef2feea3f
NelsonNV <nelsonluisnv2@gmail.com>
2025-01-07 17:46:44 -0300

    refactor: iconos ahora solo uno puede estar activo

--- core/models.py
+++ core/models.py
@@ -10,12 +10,22 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Iconos(models.Model):
+class Iconos(BaseModel):
     nombre = models.CharField(max_length=100)
     titulo = models.CharField(max_length=100)
     icono = models.FileField(upload_to="iconos/")
     logo = models.FileField(upload_to="iconos/")
     activo = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Ícono"
+        verbose_name_plural = "Íconos"
+
+    def save(self, *args, **kwargs):
+        # Si este registro se marca como activo, desactiva los demás
+        if self.activo:
+            Iconos.objects.filter(activo=True).exclude(pk=self.pk).update(activo=False)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.nombre}"
