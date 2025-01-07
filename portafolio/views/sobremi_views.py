3e0f99340102affa7248bcaf26d12a74bbef09e2
NelsonNV <nelsonluisnv2@gmail.com>
2025-01-07 17:55:22 -0300

    feat: redirigir a editar si ya existe datos
    redireciona editar sobre mi si ya tiene un dato porque solo puede
    existir uno

--- portafolio/views/sobremi_views.py
+++ portafolio/views/sobremi_views.py
@@ -1,13 +1,20 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from core.views import BaseCreateView, BaseUpdateView, BaseDeleteView, BaseDetailView
 from portafolio.models import SobreMi
 from portafolio.forms import SobreMiForm
+from django.urls import reverse
 
 class SobreMiCreateView(BaseCreateView):
     model = SobreMi
     form_class = SobreMiForm
     success_url = "/"
     title = "Crear Sobre Mí"
+
+    def dispatch(self, request, *args, **kwargs):
+        if SobreMi.objects.exists():
+            return HttpResponseRedirect(reverse('sobre_mi_editar'))
+        return super().dispatch(request, *args, **kwargs)
+
 
 class SobreMiUpdateView(BaseUpdateView):
     model = SobreMi
